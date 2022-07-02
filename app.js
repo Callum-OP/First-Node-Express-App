0864f8deb78e4c8a63f249f42f265cb22730bee8
Callum O'Prey <OPrey-C8@ulster.ac.uk>
2022-07-02 18:53:30 +0100

    Starting with a basic app

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(express.urlencoded());
 
-var app = express();
+app.get('/', function(req, res, next){
+    res.send(`
+		<div class="container">
+			<h1>To Do List</h1>
+			<div>
+				<div>Add Task</div>
+				<div>
+					<form method="POST" action="/">
+						<div>
+							<label>Task</label>
+							<input type="text" name="task" id="task" class="form-control" />
+						</div>
+		                <div class="mb-3">
+		                	<input type="submit" name="submit_button" class="btn btn-primary" value="Add" />
+		                </div>
+					</form>
+				</div>
+			</div>
+		</div>
+	`);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.post('/', function(req, res, next){
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	res.send(req.body);
+
 });
 
-module.exports = app;
+app.listen(3000);
+
+
